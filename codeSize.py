ec1e1ff408c9f4574a35f8e732c71b98686236fb
Peter Sýs <psppszavinacmnenepis@gmail.com>
2018-11-07 20:26:13 +0100

    Add support for codesize and demangle c++ symbols

--- codeSize.py
+++ codeSize.py
@@ -1,9 +1,26 @@
 #!/usr/bin/python3
 import subprocess
 import sys
+import html
+
+def bytes_to_human(byte):
+    if byte == 0:
+        return "0b"
+    res = ""
+    unit = 0
+    unitsList = ["B", "kiB", "MiB", "GiB", "TiB", "PiB", "EiB", "ZiB", "YiB"]
+    while (byte > 0) and (unit < len(unitsList)-1):
+        if res:
+            res = " " + res
+        res = "%d%s"%(byte%1024, unitsList[unit]) + res
+        unit += 1
+        byte = byte//1024
+    return res
 
 def main():
-    out = subprocess.check_output("nm --demangle --print-size --format posix --".split()+sys.argv[1:], universal_newlines=True)
+    command = "nm --demangle=gnu --print-size --format bsd -- " + " ".join(sys.argv[1:]) + "|c++filt -_"
+    out = subprocess.check_output(["sh", "-c", command], universal_newlines=True)
+    out = html.escape(out)
 
 
     print("var dataCodeSize = [")
@@ -12,15 +29,17 @@ def main():
     for line in out.splitlines():
         if not line:
             continue
-        col = line.split();
-        if (len(col) != 4) or (col[1] not in "wWtT"):
+        col = line.split(maxsplit=3);
+        if (len(col) != 4) or (col[2] not in "wWtT"):
             continue
 
         print("    {")
+        lenByte = int(col[1], base=16)
         print("""\
-        "symbol": "%s",
+        "symbol": '%s',
         "size": "%d",
-        """ % (col[0], int(col[3], base=16)))
+        "sizeHuman": "%s",
+        """ % (col[3], lenByte, bytes_to_human(lenByte)))
         print("    },")
 
 
